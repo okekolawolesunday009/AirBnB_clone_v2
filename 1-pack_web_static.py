19f6a3b100e7a1dd30895f8969fc751f9367c595
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-12-11 21:30:56 +0000

    ctx

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz archive"""
-from fabric.api import local
+from fabric import task
+from invoke import run
 from datetime import datetime
 
 
-def do_pack():
+@task
+def do_pack(ctx):
     """Generates a .tgz archive from web_static"""
     try:
         current_time = datetime.utcnow()
         formatted_time = current_time.strftime("%Y%m%d%H%M%S")
         archive_path = "versions/webstatic{}.tgz".format(formatted_time)
-        local("mkdir -p versions")
-        local("tar -cvzf {} web_static".format(archive_path))
+        run("mkdir -p versions")
+        run("tar -cvzf {} web_static".format(archive_path))
         print("web_static packed: {} -> {}Bytes".format(archive_path, local(
             "du -b {}".format(archive_path), capture=True)))
         return archive_path
