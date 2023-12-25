b8cc128edb23e892377d965b053e7339a1a37fc1
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-12-25 16:34:17 +0000

    betty

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,21 +9,24 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def is_fun(text):
     f_text = text = text.replace('_', ' ')
     return f'C {f_text}'
 
+
 @app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python(text= 'is cool'):
+def python(text='is cool'):
     f_text = text.replace('_', ' ')
     return f"Python {f_text}"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')                                         
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -10,20 +10,24 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def is_fun(text):
     f_text = text = text.replace('_', ' ')
     return f'C {f_text}'
 
+
 @app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python(text= 'is cool'):
+def python(text='is cool'):
     f_text = text.replace('_', ' ')
     return f"Python {f_text}"
+
 
 @app.route("/number/<n>", strict_slashes=False)
 def number(n):
@@ -32,10 +36,12 @@ def number(n):
         return f"{n} is a number"
     except ValueError:
         pass
+
+
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')  
+    app.run(host='0.0.0.0', port='5000')
