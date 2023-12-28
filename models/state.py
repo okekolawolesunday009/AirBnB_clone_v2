459ac9599b167d00a33462edda45dc45c3ced088
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-12-28 09:50:04 +0000

    len fixing

--- models/state.py
+++ models/state.py
@@ -17,8 +17,7 @@ class State(BaseModel, Base):
     cities = relationship(
             'City',
             backref='states',
-            cascade='all,
-            delete-orphan'
+            cascade='all,delete-orphan'
             )
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
