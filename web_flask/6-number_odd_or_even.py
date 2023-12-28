df1575e5e8fcdbf8d7d28029f8cfd2885d821b2e
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-12-28 09:20:05 +0000

    fix 6-numberEvenOrOdd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -43,7 +43,7 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route("/number_odd_or_even/<n>", strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     result = f"{'odd' if int(n) % 2 != 0 else 'even'}"
     return render_template('6-number_odd_or_even.html', n=n, result=result)
