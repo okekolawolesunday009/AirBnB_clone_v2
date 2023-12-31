095cc12550eed69c45b384c3208aca42b789cb37
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-12-31 18:10:39 +0000

    re-add del file

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python3
-"""start python app
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+"to display 7-states_list.html"
 """
-from flask import Flask
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
 def states():
-    """display states"""
+    """Displays an HTML page with a list of all States.
+    States are sorted by name.
+    """
     states = storage.all(State)
-    return render_template('9-states.html', states=states)
+    return render_template("9-states.html", state=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
-def states_id(id=None):
-    """display states based on id if any"""
-    states = storage.all(State).values()
-    for state in states:
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all(State).values():
         if state.id == id:
-            return render_template(
-                    "9-states.html",
-                    state=state,
-                    )
+            return render_template("9-states.html", state=state)
     return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
