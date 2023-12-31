a287ebb5a0322332e59ca7210f574e5d73b659e7
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-12-31 17:36:35 +0000

    new year

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route("/states", strict_slashes=False)
 def states():
     """display states"""
-    states = storage.all(State).values()
+    states = storage.all(State)
     return render_template('9-states.html', states=states)
 
 
@@ -20,15 +20,14 @@ def states_id(id=None):
     """display states based on id if any"""
     states = storage.all(State).values()
     for state in states:
+        print(state.id)
         if state.id == id:
+            print(state.id)
             return render_template(
                     "9-states.html",
                     state=state,
                     )
         return render_template("9-states.html")
-
-
-
 
 
 @app.teardown_appcontext
